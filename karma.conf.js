ddb2186595920c82b5520b462af1042ee6b69f2f
S.SATHYA NARAYAN <sathyanarayan.mec09@gmail.com>
2020-12-06 20:39:10 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -57,6 +57,12 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
 
 
     // Continuous Integration mode
